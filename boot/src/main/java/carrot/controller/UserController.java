8772384e78f2b31c38fbca23f321bbdc7fd7b9d4
王宇 <yu.wangy@tongdun.cn>
2019-01-27 12:27:40 +0800

    spring boot demo

--- boot/src/main/java/carrot/controller/UserController.java
+++ boot/src/main/java/carrot/controller/UserController.java
@@ -2,6 +2,8 @@ package carrot.controller;
 
 import carrot.bean.User;
 import carrot.dto.Result;
+import carrot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,8 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class UserController {
 
+
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "hello",method = RequestMethod.GET)
-    public String test(){
+    public String test(HttpServletRequest request){
+        request.getSession();
         return "hello world";
     }
 
@@ -19,4 +26,11 @@ public class UserController {
         System.out.println(user.getName());
         return Result.successRs();
     }
+
+    @RequestMapping(value = "/user/{name}",method = RequestMethod.GET)
+    public User findUser(HttpServletRequest request,@PathVariable("name") String  name){
+        return userService.getUser(name);
+    }
+
+
 }
